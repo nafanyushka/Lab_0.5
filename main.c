a9edccd9f02e8eadfc3215b00b5c3cfc08740e9c
Nafany <bausov.vadim@yandex.ru>
2021-03-09 00:40:37 +0300

    Full reverse

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 char* get_String();
+char* reverse_words(char*);
 
 int main(){
 
@@ -11,6 +12,8 @@ int main(){
     while(*a != '\0'){
         a = get_String();
         printf("%s\n", a);
+        char* b = reverse_words(a);
+        printf("REVERSED: %s\n", b);
     };
     system("pause");
     return 0;
@@ -40,5 +43,18 @@ char* get_String(){
 }
 
 char* reverse_words(char* string){
-    
+    int size = strlen(string);
+    char* a = (char*)malloc(size * sizeof(char));
+    if(a == NULL)
+    {
+        printf("A BCE!\n");
+        return 0;
+    }
+    for(int i = size - 1; i >= 0; i--, a++){
+        *a = *(string + i);
+        printf("%c\n", *a);
+    }
+    *(a) = '\0';
+//    scanf("%*[^\n]");
+    return a - size;
 }
